5f4ca5ceb5ea3d391db6e0a3120c76271a990515
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-01-14 17:48:22 -0700

    committing to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const htmlRoutes = require('./routes/html-routes');
 const apiRoutes = require('./routes/api-routes');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
